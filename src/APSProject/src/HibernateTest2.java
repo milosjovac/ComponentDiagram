e12ab3fd2e1ae9e4fa62cc8fb3def829ff3891ab
uberchilly <uberchilly@gmail.com>
2013-12-04 22:15:33 +0100

    Nije depracated vise.

--- src/APSProject/src/HibernateTest2.java
+++ src/APSProject/src/HibernateTest2.java
@@ -1,6 +1,9 @@
+
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistryBuilder;
 
 import com.aps.dmo.Komponenta;
 
@@ -9,6 +12,8 @@ public class HibernateTest2 {
 	/**
 	 * @param args
 	 */
+	private static SessionFactory sessionFactory;
+	private static org.hibernate.service.ServiceRegistry serviceRegistry;
 	public static void main(String[] args) {
 		Komponenta kom1 = new Komponenta();
 		kom1.setIdKomponente(2);
@@ -20,6 +25,16 @@ public class HibernateTest2 {
 		kom1.setStereotip(true);
 		kom1.setDekoracija(true);
 
+		Configuration configuration = new Configuration();
+	    configuration.configure();
+	    serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();        
+	    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+	    
+	    Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(kom1);
+		session.getTransaction().commit();
+	    /*
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
@@ -27,7 +42,7 @@ public class HibernateTest2 {
 		session.save(kom1);
 		
 		session.getTransaction().commit();
-		
+		*/
 	}
 
 }
